b81965246975ed95c42276e2ea8cd9709dd39cce
hsafford <hannahrsafford@gmail.com>
2026-07-07 10:50:14 -0400

    Set --header-h dynamically from actual header height for sticky offsets

--- Resources/Scripts/Script.js
+++ Resources/Scripts/Script.js
@@ -2,3 +2,12 @@ import * as Cmpnt from './Components.js'
 
 Cmpnt.ProcessHeader()
 Cmpnt.ProcessFooter()
+
+function updateHeaderHeightVar(){
+  const header = document.querySelector('header')
+  if(!header) return
+  document.documentElement.style.setProperty('--header-h', header.offsetHeight + 'px')
+}
+updateHeaderHeightVar()
+window.addEventListener('resize', updateHeaderHeightVar)
+window.addEventListener('load', updateHeaderHeightVar)
